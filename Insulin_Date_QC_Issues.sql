25764f78458d9f5339f25625369c27f8704e59f1
jhhsu8 <jhhsu11@gmail.com>
2020-03-16 22:15:29 -0700

    Update Insulin_Date_QC_Issues.sql

--- Insulin_Date_QC_Issues.sql
+++ Insulin_Date_QC_Issues.sql
@@ -1,7 +1,5 @@
 --Insulins Date QC Issues
 
---Please see the attached spreadsheet for the list of mice with incorrect dates.
-
 --Dates of Experiment: 2/2/2015 – 5/8/2019
 
 --Note: Plasma samples can be frozen and used for insulin analysis at a later date.
@@ -14,13 +12,15 @@ UPDATE insulins INNER JOIN necropsies ON necropsies.mouse_name = insulins.mouse_
 SET insulins.blood_collection_datetime = necropsies.datetime, insulins.modified = '2020-02-05 11:00:00'
 WHERE necropsies.datetime = insulins.datetime
   AND insulins.blood_collection_datetime > insulins.datetime
- Change those incorrect insulin datetimes to the correct blood collection dates (I have checked that necropsies dates are the same as Mosaic death dates).
+  
+--Change those incorrect insulin datetimes to the correct blood collection dates (I have checked that necropsies dates are the same as Mosaic death dates).
 
 UPDATE insulins INNER JOIN necropsies ON necropsies.mouse_name = insulins.mouse_name
 SET insulins.datetime = insulins.blood_collection_datetime, insulins.modified = '2020-02-05 11:47:00'
 WHERE insulins.blood_collection_datetime > insulins.datetime
   AND DATE(insulins.blood_collection_datetime) = DATE(necropsies.datetime)
- Fix the mismatched Mosaic death dates, necropsies dates, insulin datetimes, insulin blood collection dates which are after insulin datetimes (see the results from the two SQL queries below).
+
+--Fix the mismatched Mosaic death dates, necropsies dates, insulin datetimes, insulin blood collection dates which are after insulin datetimes (see the results from the two SQL queries below).
 
 SELECT insulins.mouse_name,
        ltam.death_date,
@@ -42,9 +42,8 @@ FROM insulins
 WHERE insulins.blood_collection_datetime > insulins.datetime;
 
 --2. Blood collection date is more than 14 days earlier or later than timepoint specified by impress:
---Mosaic death dates, insulin datetimes, insulin blood collection dates are mismatched.
 
---Status code those incorrect blood collection dates as shown in the results from the SQL queries below
+--Status code those incorrect blood collection dates as shown in the results from the SQL query below
  
 SELECT insulins.mouse_name, ltam.death_date, insulins.blood_collection_datetime, insulins.datetime, insulins.modified
 FROM insulins
