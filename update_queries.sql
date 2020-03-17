4f13e5bad31e560324f9e748cc0fdb4cb9e08867
jhhsu8 <jhhsu11@gmail.com>
2020-03-16 22:16:03 -0700

    Delete update_queries.sql

--- update_queries.sql
+++
@@ -1,7 +0,0 @@
--- set hematology blood collection and sacrifice dates to necropsies dates.
-UPDATE hemas INNER JOIN necropsies ON necropsies.mouse_name = hemas.mouse_name
-SET hemas.collection_datetime = necropsies.datetime,
-    hemas.sacrifice_datetime  = necropsies.datetime
-WHERE hemas.collection_datetime > hemas.datetime 
-  AND hemas.sacrifice_datetime > hemas.datetime 
-  AND necropsies.datetime <= hemas.datetime;
